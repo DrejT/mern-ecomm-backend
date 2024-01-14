c237539fdfb4f9c26d0209dd86a2c56ab55865ab
DrejT <119151870+DrejT@users.noreply.github.com>
2024-01-14 15:19:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(express.json());
 const corsOptions = {
   origin: process.env.DOMAIN_NAME,
   credentials: true,
-  preflightContinue:true,
 };
 app.use(helmet());
 app.use(cors(corsOptions));
