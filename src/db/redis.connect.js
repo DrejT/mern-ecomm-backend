d6006ca87426f0fac76ef9e76f0b36f29247fef1
DrejT <vivekpatel4049@gmail.com>
2024-01-14 20:31:06 +0530

    update

--- src/db/redis.connect.js
+++ src/db/redis.connect.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const redis = require("redis");
 
 const client = redis.createClient({
-  url: `rediss://default:${process.env.REDIS_PASSWORD}@special-ox-46357.upstash.io:463577`,
+	url:`rediss://default:${process.env.REDIS_PASSWORD}@eu2-tops-quagga-31391.upstash.io:31391`
 });
 
 (async () => {
