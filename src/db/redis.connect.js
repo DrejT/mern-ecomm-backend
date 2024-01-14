ef1c61339a0edf37d6412fd20470b6ffca32f5e4
DrejT <vivekpatel4049@gmail.com>
2024-01-14 19:58:37 +0530

    update

--- src/db/redis.connect.js
+++ src/db/redis.connect.js
@@ -1,19 +1,18 @@
-require("dotenv").config()
+require("dotenv").config();
 const redis = require("redis");
 
 const client = redis.createClient({
-	url:`rediss://default:${process.env.REDIS_PASSWORD}@eu2-tops-quagga-31391.upstash.io:31391`
+  url: `rediss://default:${process.env.REDIS_PASSWORD}@special-ox-46357.upstash.io:463577`,
 });
 
 (async () => {
   await client
-  .on('connect', () => console.log("redis connected"))
-  .on('ready', () => console.log("redis is now ready"))
-  .on('reconnecting', () => console.log("reconnecting redis"))
-  .on('error', err => console.log('Redis Client Error', err))
-  .on('end', () => console.log("redis has stopped"))
-  .connect();
+    .on("connect", () => console.log("redis connected"))
+    .on("ready", () => console.log("redis is now ready"))
+    .on("reconnecting", () => console.log("reconnecting redis"))
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .on("end", () => console.log("redis has stopped"))
+    .connect();
 })();
-
 
 module.exports = client;
